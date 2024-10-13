a264efef52330725601696e4baa3d27ca21adb0e
Hamdullahkhan121 <hamdullahkhan121@gmail.com>
2024-10-14 00:54:47 +0500

    4th commit: end of Day 1

---
+++ DayOne/finalProjNameGen.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand name Generator")
+cityName = input("Whats the name of city you grew up in? \n")
+petName = input("What is your pet name? \n")
+print("Your Brand Name is "+ cityName +" "+ petName)
